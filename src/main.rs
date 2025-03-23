e5f26a01e990ba95b5985e608edb66fa9d4d6eae
amdryzen5600x <arca26100@gmail.com>
2025-03-23 19:27:40 +0500

    feat: introduce threadpool to handle several connections simultaniously

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rayon::spawn;
 use std::{
     collections::HashMap,
     io::Result,
@@ -15,24 +16,27 @@ fn main() -> Result<()> {
     let data: Data = Arc::new(Mutex::new(HashMap::new()));
     let listener = TcpListener::bind("127.0.0.1:6969")?;
     for stream in listener.incoming() {
-        println!(".");
-        if let Ok(mut stream) = stream {
-            loop {
-                let mut buf = [0];
-                let by = stream.peek(&mut buf);
-                if let Ok(by) = by {
-                    if by == 0 {
-                        break;
+        let data = data.clone();
+        spawn(move || {
+            println!(".");
+            if let Ok(mut stream) = stream {
+                loop {
+                    let mut buf = [0];
+                    let by = stream.peek(&mut buf);
+                    if let Ok(by) = by {
+                        if by == 0 {
+                            break;
+                        }
+                    }
+                    let request = parse::parse(&mut stream);
+                    println!("{:?}", request);
+                    if let Ok(req) = request {
+                        let _ = route(req, &mut stream, data.clone());
                     }
                 }
-                let request = parse::parse(&mut stream);
-                println!("{:?}", request);
-                if let Ok(req) = request {
-                    route(req, &mut stream, data.clone())?;
-                }
             }
-        }
-        println!("connection is about to be lost")
+            println!("connection is about to be lost")
+        });
     }
     Ok(())
 }
